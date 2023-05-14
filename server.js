3f67a19b30d98eec914641c40b81e36ab35e1731
aarontravass <aaron.travass@hotmail.com>
2023-05-14 18:21:32 -0400

    commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = new App().use(lruSend());
 
 app.get('/weather/search', async (req, res) => {
     const city = req.query.city;
+    req.ip
     const cacheResult = await getWeatherByCity(city);
     return res.status(200).send(cacheResult);
 })
